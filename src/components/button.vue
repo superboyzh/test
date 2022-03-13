65b558d9552ed652b7a1bba26d470e4b7d070d0f
superboyzh <1431814914@qq.com>
2022-03-13 15:57:31 +0800

    第一次提交

---
+++ src/components/button.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-card>
+    <el-button type="danger">aaa</el-button>
+    <el-button type="warning">aaa</el-button>
+    <el-button type="primary">aaa</el-button>
+    <el-button type="success">aaa</el-button>
+  </el-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="root">
+    <!-- <h2 v-if="a">你好啊</h2> -->
+    <h2 :style="{ opacity }">欢迎学习Vue</h2>
+    <button @click="stop">点我停止</button>
+  </div>
+</template>
+
+<script>
+// import aaa from '../components/button.vue'
+  export default {
+    // 
+  // components:{
+  //     aaa
+  // },
+
+  data() {
+    return {
+      // a:false,
+      opacity: 1,
+    };
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  mounted() {
+    console.log("mounted", this);
+    this.timer = setInterval(() => {
+      console.log("setInterval开启了");
+      this.opacity -= 0.01;
+      if (this.opacity <= 0) this.opacity = 1;
+    }, 16);
+  },
+  methods: {
+    stop() {
+      this.$destroy();
+    },
+  },
+  beforeDestroy() {
+    console.log("beforeDestroy"), clearInterval(this.timer);
+  },
+};
+</script>
+
+<style scoped>
+</style>>
